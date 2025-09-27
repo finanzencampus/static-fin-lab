51675bbaa77d8cc8c0944a78d060d52a951886cd
finanzencampus <julian.sieburg22@gmail.com>
2025-09-27 21:28:33 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "@/components/Header";
 import Index from "./pages/Index";
 import Instruments from "./pages/Instruments";
-import Portfolio from "./pages/Portfolio";
 import Quiz from "./pages/Quiz";
 import Glossary from "./pages/Glossary";
 import Calculator from "./pages/Calculator";
@@ -26,7 +25,6 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/instruments" element={<Instruments />} />
-              <Route path="/portfolio" element={<Portfolio />} />
               <Route path="/quiz" element={<Quiz />} />
               <Route path="/glossary" element={<Glossary />} />
               <Route path="/calculator" element={<Calculator />} />
